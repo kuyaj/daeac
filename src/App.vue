85a438487706653864dc5c638b446a244c1e317a
Angelo <valmocinajayangelo@gmail.com>
2022-08-01 13:38:46 +0800

    added vue-barcode-reader

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <barcode-reader></barcode-reader>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BarcodeReader from './components/BarcodeReader.vue';
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    "barcode-reader": BarcodeReader
   }
 }
 </script>
---
+++ src/components/BarcodeReader.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+     <StreamBarcodeReader
+        @decode="onDecode"
+        @loaded="onLoaded"
+      ></StreamBarcodeReader>
+{{ state.message }}
+  </div>
+</template>
+
+<script>
+import { StreamBarcodeReader } from "vue-barcode-reader";
+import { reactive } from 'vue';
+
+export default {
+  name: 'BarcodeReader',
+  setup(){
+    let state = reactive({ message: "let it begin!"})
+
+    let onDecode = function(text){
+      console.log(`Decode text from QR code is ${text}`);
+    }
+
+    let onLoaded = function() {
+      console.log(`Ready to start scanning barcodes`);
+    }
+
+    return {
+      state,
+      onDecode,
+      onLoaded
+    }
+  },
+  components: {
+    StreamBarcodeReader
+  }
+}
+</script>
