e752daa582643930989fd4be1b7a22a68a2eb40e
Angelo <valmocinajayangelo@gmail.com>
2022-08-06 09:30:06 +0800

    added a qr generator component

--- src/components/BarcodeReader.vue
+++ src/components/BarcodeReader.vue
@@ -1,21 +1,35 @@
 <template>
   <div>
-     <StreamBarcodeReader
+      <div class="qr-reader">
+        <qr-reader
         @decode="onDecode"
         @loaded="onLoaded"
-      ></StreamBarcodeReader>
-{{ state.message }}
+      ></qr-reader>
+      </div>
+
+        <input type="text" v-model="state.qr">
+
+      <br>
+      <div class="qr-code">
+          <qr-code 
+            :text="state.qr" 
+          ></qr-code>
+      </div>
+      <br>
+{{ state.message }} 
+<br>
   </div>
 </template>
 
 <script>
-import { StreamBarcodeReader } from "vue-barcode-reader";
+// import { StreamBarcodeReader } from "vue-barcode-reader";
+import VueQRCodeComponent from 'vue-qr-generator';
 import { reactive } from 'vue';
 
 export default {
   name: 'BarcodeReader',
   setup(){
-    let state = reactive({ message: "QR decoded here.."})
+    let state = reactive({ message: "QR decoded here..", qr: ""})
 
     let onDecode = function(text){
       // alert('The decoded qr code is '+text);
@@ -34,7 +48,21 @@ export default {
     }
   },
   components: {
-    StreamBarcodeReader
+    "qr-code": VueQRCodeComponent
   }
 }
 </script>
+<style>
+
+.qr-reader {
+  border: 1px solid #2ecc71;
+  padding: 10px 10px;
+}
+
+.qr-code {
+  border: 1px solid #3498db;
+  padding: 10px 10px;
+  text-align: center;
+}
+</style>
+
